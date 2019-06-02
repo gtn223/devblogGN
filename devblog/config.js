162aba7591d341b2135c53e118f586efae7a00a4
Grant Ng <gtn223@nyu.edu>
2019-06-02 18:42:58 -0400

    new blog-post

---
+++ devblog/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  title: 'Dev Blog', // Required
+  author: 'Grant Ng', // Required
+  description: 'Front End Developer',
+  primaryColor: '#3498db', // Required
+  showHeaderImage: true,
+  showShareButtons: true,
+  postsPerPage: 5, // Required
+  social: {
+    website: 'https://gtn223.github.io/PortfolioGrantKing/',
+    github: 'https://github.com/ryanfitzgerald',
+    twitter: 'https://twitter.com/ryanafitzgerald',
+    linkedin: 'https://ca.linkedin.com/in/ryanafitzgerald',
+  },
+  pathPrefix: '/devblog',
+  siteUrl: 'https://ryanfitzgerald.github.io/devblog/',
+};
